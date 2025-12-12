14330a09cfd1bc25d7116f30968b615b366b936e
Sanika Kangane <sanikakangane166@gmail.com>
2025-12-12 19:43:20 +0530

    Add files via upload

---
+++ mailing_address.py
@@ -0,0 +1,7 @@
+print("Sanika Gorakhnath Kangane")
+print("Flat No. 903, SAI SATYAM HOMES")
+print("Umberde Road, Pehlwaan Chowk")
+print("kalyan (WEST) = 421301")
+print("Maharashtra")
+print("India")
+
